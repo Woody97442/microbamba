f7e1d795b2b2cef3fba6459a85e0862643e61bf9
Woody97442 <woody97442@hotmail.fr>
2024-05-13 20:24:07 +0400

    update securiter app and add .env

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 import Pages from 'vite-plugin-pages'
+import EnvironmentPlugin from 'vite-plugin-environment'
+
 
 export default defineConfig({
   plugins: [react(), Pages({
     dirs: 'src/pages',
-  })],
+  }), EnvironmentPlugin(['STRAPI_KEY', 'API_URL', 'API_IMAGE_URL']),],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
