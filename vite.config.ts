524648dc85c633a34374ac9230b1404b06ac6d84
brianosipro <woody97442@hotmail.fr>
2024-05-16 12:13:50 +0400

    add config redirect rout

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-import Pages from 'vite-plugin-pages'
 import EnvironmentPlugin from 'vite-plugin-environment'
 
-import process from 'process'
 
 export default defineConfig({
-  plugins: [react(), Pages({
-    dirs: 'src/pages',
-  }), EnvironmentPlugin(['VITE_STRAPI_KEY', 'VITE_API_URL', 'VITE_API_IMAGE_URL']),],
+  plugins: [react(), EnvironmentPlugin(['VITE_STRAPI_KEY', 'VITE_API_URL', 'VITE_API_IMAGE_URL']),],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
